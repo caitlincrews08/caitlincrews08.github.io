d602f115c0f0f2cec94ab7e8f044097686d51177
Caitlin Crews <caitlinrcrews@gmail.com>
2020-10-03 23:39:47 -0400

    added if/else conditions

--- Homework/hw-3/develop/script.js
+++ Homework/hw-3/develop/script.js
@@ -6,7 +6,7 @@ var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
 "s", "t", "u", "v", "w", "x", "y", "z"];
 var cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
 "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var char = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
 "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
@@ -16,8 +16,8 @@ console.log(lowLength);
 var capLength = cap.length;
 console.log(capLength);
 
-var numLength = num.length;
-console.log(numLength);
+var numbersLength = numbers.length;
+console.log(numbersLength);
 
 var charLength = char.length;
 console.log(charLength);
@@ -37,20 +37,35 @@ if (length < 8 || length > 128) {
 if (!lower && !upper && !num && !specialchar) {
   alert("You did not select any password criteria! Try again.")
   location.reload();
-} else if (lower | upper | num | specialchar) {
+} else if (lower | upper | num | specialcharacter) {
     password = generatePassword();
 }
 
 function generatePassword() {
 
+  var complexity = length.value;
+
+  for (var i = 0; i <= complexity; i++) {
+    if (lower) {
+      password = password + low[Math.floor(Math.random() * low.length)];
+    }
+    else if (upper) {
+      password = password + cap[Math.floor(Math.random() * cap.length)];
+    }
+    else if (num) {
+      password = password + numbers[Math.floor(Math.random() * numbers.length)];
+    }
+    else if (specialcharacter) {
+      password = password + char[Math.floor(Math.random() * char.length)];
+    }
+  }
 }
+
+generatePassword();
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword()
-  var complexity = length.value;
-
-  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
