93ffa4587bb1ccc051bd49f66a2240fd7d4519ce
Caitlin Crews <caitlinrcrews@gmail.com>
2020-10-03 23:05:09 -0400

    updated password values

--- Homework/hw-3/develop/script.js
+++ Homework/hw-3/develop/script.js
@@ -2,13 +2,25 @@
 var generateBtn = document.querySelector("#generate");
 
   // password values
-var alphaLow = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
-"s", "t", "u", "v", "w", "x", "y", "z"]
-var alphaUp = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "K", "L", "M", "N", "O", "P", "Q", "R",
-"S", "T", "U", "V", "W", "X", "Y", "Z"]
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
-// var specialchar = (" !"#$%&'()*+,-./:;<=>?@[\]^_`{|}~") 
+var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r",
+"s", "t", "u", "v", "w", "x", "y", "z"];
+var cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
+"S", "T", "U", "V", "W", "X", "Y", "Z"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var char = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
+"<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
+var lowLength = low.length;
+console.log(lowLength);
+
+var capLength = cap.length;
+console.log(capLength);
+
+var numLength = num.length;
+console.log(numLength);
+
+var charLength = char.length;
+console.log(charLength);
 
 
 var length = prompt("Enter desired password length: 8 - 128 characters", length);
@@ -20,21 +32,25 @@ if (length < 8 || length > 128) {
   var lower = confirm("Would you like to include lowercase letters?");
   var upper = confirm("Would you like to include uppercase letters?");
   var num = confirm("Would you like to include numbers?");
-  var specialchar = confirm("Would you like to include special characters?");
+  var specialcharacter = confirm("Would you like to include special characters?");
 };
-
-
 if (!lower && !upper && !num && !specialchar) {
   alert("You did not select any password criteria! Try again.")
   location.reload();
-} else if (lower === true) {
-    password = generatePassword(Math.floor(Math.random(alphaLow)));
+} else if (lower | upper | num | specialchar) {
+    password = generatePassword();
 }
 
+function generatePassword() {
+
+}
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = generatePassword()
+  var complexity = length.value;
+
+  
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
