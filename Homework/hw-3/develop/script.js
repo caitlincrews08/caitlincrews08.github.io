bebf3cd9a91ecd8bc87eab51ba3a83c93a47ddb6
Caitlin Crews <caitlinrcrews@gmail.com>
2020-10-05 00:30:41 -0400

    updated all conditional statements and fixed randomizing component

--- Homework/hw-3/develop/script.js
+++ Homework/hw-3/develop/script.js
@@ -6,62 +6,92 @@ var low = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n",
 "s", "t", "u", "v", "w", "x", "y", "z"];
 var cap = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R",
 "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var num = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var char = [" ", "!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";",
 "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", "}", "~"];
 
-var lowLength = low.length;
-console.log(lowLength);
-
-var capLength = cap.length;
-console.log(capLength);
-
-var numbersLength = numbers.length;
-console.log(numbersLength);
-
-var charLength = char.length;
-console.log(charLength);
-
-
-var length = prompt("Enter desired password length: 8 - 128 characters", length);
-if (length < 8 || length > 128) {
-  alert("Enter a valid length.")
-  location.reload();
-} else if (length > 8 && length < 128) {
-  console.log(length)
-  var lower = confirm("Would you like to include lowercase letters?");
-  var upper = confirm("Would you like to include uppercase letters?");
-  var num = confirm("Would you like to include numbers?");
-  var specialcharacter = confirm("Would you like to include special characters?");
-};
-if (!lower && !upper && !num && !specialchar) {
-  alert("You did not select any password criteria! Try again.")
-  location.reload();
-} else if (lower | upper | num | specialcharacter) {
-    password = generatePassword();
-}
-
 function generatePassword() {
+  // determining password length
+  var length = prompt("Enter desired password length: 8 - 128 characters");
 
-  var complexity = length.value;
+  if (length < 8 || length > 128) {
+    alert("Enter a valid length.");
 
-  for (var i = 0; i <= complexity; i++) {
-    if (lower) {
-      password = password + low[Math.floor(Math.random() * low.length)];
-    }
-    else if (upper) {
-      password = password + cap[Math.floor(Math.random() * cap.length)];
-    }
-    else if (num) {
-      password = password + numbers[Math.floor(Math.random() * numbers.length)];
-    }
-    else if (specialcharacter) {
-      password = password + char[Math.floor(Math.random() * char.length)];
-    }
+  } else if (length > 7 && length < 129) {
+    console.log(length);
+
+    // continues with criteria questions
+    var includeLow = confirm("Would you like to include lowercase letters?");
+    var includeCap = confirm("Would you like to include uppercase letters?");
+    var includeNum = confirm("Would you like to include numbers?");
+    var includeChar = confirm("Would you like to include special characters?");
+  };
+
+  var options;
+  // no criteria was chosen
+  if (!includeLow && !includeCap && !includeNum && !includeChar) {
+    options = alert("You did not select any password criteria! Try again.")
+    location.reload();
+
+    // all 4 criteria chosen
+  } else if (includeLow && includeCap && includeNum && includeChar) {
+    options = low.concat(cap, num, char);
+
+    // 3 criteria chosen
+  }else if (includeLow && includeCap && includeNum) {
+    options = low.concat(cap, num);
+
+  }else if (includeLow && includeCap && includeChar) {
+    options = low.concat(cap, char);
+
+  }else if (includeLow && includeNum && includeChar) {
+    options = low.concat(num, char);
+
+  }else if (includeCap && includeNum && includeChar) {
+    options = cap.concat(num, char);
+  
+  // 2 criteria chosen
+  }else if (includeLow && includeCap) {
+    options = low.concat(cap);
+  
+  }else if (includeLow && includeNum) {
+    options = low.concat(num);
+  
+  }else if (includeLow && includeChar) {
+    options = low.concat(char);
+
+  }else if (includeCap && includeNum) {
+    options = cap.concat(num);
+
+  }else if (includeCap && includeChar) {
+    options = cap.concat(char);
+
+  }else if (includeNum && includeChar) {
+    options = num.concat(char);
+
+    // 1 criteria chosen
+  }else if (includeLow) {
+    options = low;
+
+  }else if (includeCap) {
+    options = cap;
+
+  }else if (includeNum) {
+    options = num;
+
+  }else if (includeChar) {
+    options = char;
+  };
+
+
+  // randomizes options
+  for (var i = 0; i < length; i++) {
+    password = options[Math.floor(Math.random() * options.length)];
+    console.log(password);
   }
-}
 
-generatePassword();
+  
+};
 
 // Write password to the #password input
 function writePassword() {
@@ -69,8 +99,8 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
+writePassword();
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
